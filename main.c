16a29a010f1f1d3ed47e9f44f44db06dee0f546b
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2022-09-24 22:22:41 +0300

    add CenterWindow function

--- main.c
+++ main.c
@@ -3,18 +3,74 @@
 #include <windows.h>
 #include <tchar.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PWSTR szCmdLine, int CmdShow) {
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CenterWindow(HWND);
 
-    printf("Hi1\n");
-    MessageBoxW(NULL, szCmdLine, L"first Window", MB_YESNOCANCEL);
-    printf("Hi2\n");
-    return 0;
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PWSTR pCmdLine, int nCmdShow) {
+
+    MSG msg;
+    HWND hwnd;
+    WNDCLASSW wc;
+
+    wc.style = CS_HREDRAW | CS_VREDRAW ; // vertical and horizontal redrawing
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.lpszClassName = L"Game field";
+    wc.hInstance = hInstance;
+    wc.hbrBackground = GetSysColorBrush(COLOR_3DHILIGHT);
+    wc.lpszMenuName = NULL;
+    wc.lpfnWndProc = WndProc;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_ERROR);
+
+    RegisterClassW(&wc);
+    hwnd = CreateWindowW(wc.lpszClassName, L"Catch me if you can",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        100, 100, 800, 600, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+
+        DispatchMessage(&msg);
+    }
+   
+    return (int)msg.wParam;
 }
 
-int main() {
-    printf("Hi3\n");
-    wWinMain(NULL,NULL,NULL,NULL);
-    return 0;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
+    WPARAM wParam, LPARAM lParam) {
+
+    switch (msg) 
+    {
+    case WM_CREATE:
+
+        CenterWindow(hwnd);
+        break;
+
+        case WM_DESTROY:
+        {
+            PostQuitMessage(EXIT_SUCCESS);
+        }
+        return 0;
+    }
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+void CenterWindow(HWND hwnd) {
+
+    RECT rc = { 0 };
+
+    GetWindowRect(hwnd, &rc);
+    int win_w = rc.right - rc.left;
+    int win_h = rc.bottom - rc.top;
+
+    int screen_w = GetSystemMetrics(SM_CXSCREEN);
+    int screen_h = GetSystemMetrics(SM_CYSCREEN);
+
+    SetWindowPos(hwnd, HWND_TOP, (screen_w - win_w) / 2,
+        (screen_h - win_h) / 2, 0, 0, SWP_NOSIZE);
 }
 
