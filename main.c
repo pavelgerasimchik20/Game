dd66ee0d9b2abb4f6ad88ac3655613f7f9dc951e
Pavel Gerasimchik <60985318+pavelgerasimchik20@users.noreply.github.com>
2022-09-26 18:57:44 +0300

    add first animation (#4)
    * add wWinMain function to create fisrt window
    
    * add CenterWindow function
    
    * registered hot key ALT+Q to close window
    
    * add menu + hotkey for each menu item
    
    * feature: Full screen menu point
    
    * add custom animation functions

--- main.c
+++ main.c
@@ -1,26 +1,174 @@
-#define _CRT_SECURE_NO_DEPRECATE
+#pragma comment(linker, "\"/manifestdependency:type='Win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include <stdlib.h>
 #include <stdio.h>
 #include <windows.h>
-#include <wchar.h>
+#include <tchar.h>
+#define ID_HOTKEY_QUIT 1
+#define ID_HOTKEY_NEW 2
+#define IDM_NEW 1
+#define IDM_FULL 2
+#define IDM_QUIT 3
+#define masSize 5
+#define pixelMove 10 // this var will responce for speed of obj   ***TODO pick mode ???***
+RECT mas[masSize];
 
-int wmain(int argc, wchar_t** argv) {
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CenterWindow(HWND);
+void AddMenus(HWND);
+void WinShow(HDC dc);
+void InitCoordinates(void);
+void ItemMove(void);
 
-    PDWORD cChars = NULL;
-    HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
-    printf("Hi its me");
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PWSTR pCmdLine, int nCmdShow) {
 
-    if (std == INVALID_HANDLE_VALUE) {
-        wprintf(L"Cannot retrieve standard output handle\n (%d)",
-            GetLastError());
+    MSG msg;
+    HWND hwnd;
+    WNDCLASSW wc;
+
+    wc.style = CS_HREDRAW | CS_VREDRAW ; // vertical and horizontal redrawing
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.lpszClassName = L"Game field";
+    wc.hInstance = hInstance;
+    wc.hbrBackground = GetSysColorBrush(COLOR_3DHILIGHT);
+    wc.lpszMenuName = NULL;
+    wc.lpfnWndProc = WndProc;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_ERROR);
+
+    RegisterClassW(&wc);
+    hwnd = CreateWindowW(wc.lpszClassName, L"Catch me if you can",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        0, 0, 800, 600, NULL, NULL, hInstance, NULL);
+    HDC dc = GetDC(hwnd);  // getting context of the window
+
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+    UpdateWindow(hwnd);
+    InitCoordinates();
+
+    while (1) {
+        if (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE)) { // check the mess queue
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else {
+            ItemMove();
+            WinShow(dc);
+            Sleep(1);
+        }
+        //printf("msg.message: %d & msg.wParam: %d\n", msg.message, msg.wParam);  that needs to spy on codes of everything
     }
-
-    if (argv[1]) {
-
-        WriteConsoleW(std, argv[1], wcslen(argv[1]), cChars, NULL);
-    }
-    
-    CloseHandle(std);
-
+   
     return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
+    WPARAM wParam, LPARAM lParam) {
+
+    switch (msg) 
+    {
+    case WM_CREATE:
+        AddMenus(hwnd);
+        RegisterHotKey(hwnd, ID_HOTKEY_QUIT, MOD_ALT, 0x51);  // ALT + Q  to close the window
+        RegisterHotKey(hwnd, ID_HOTKEY_NEW, MOD_ALT, 0x4E);  // ALT + N  to restart
+        CenterWindow(hwnd);
+        break;
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {
+        case IDM_NEW:
+            MessageBeep(MB_ICONINFORMATION);
+            break;
+        case IDM_FULL:
+            ShowWindow(hwnd, SW_MAXIMIZE);
+            break;
+        case IDM_QUIT:
+            SendMessage(hwnd, WM_CLOSE, 0, 0);
+            break;
+        }
+        return 0;
+
+    case WM_HOTKEY:
+        if ((wParam) == ID_HOTKEY_QUIT) {
+            DestroyWindow(hwnd);
+        }
+        if ((wParam) == ID_HOTKEY_NEW) {
+            MessageBeep(MB_ICONINFORMATION);
+        }
+        break;
+    case WM_DESTROY:
+        UnregisterHotKey(hwnd, ID_HOTKEY_QUIT);
+        UnregisterHotKey(hwnd, ID_HOTKEY_NEW);
+        PostQuitMessage(EXIT_SUCCESS);    
+        break;
+    }
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+void CenterWindow(HWND hwnd) {
+
+    RECT rc = { 0 };
+
+    GetWindowRect(hwnd, &rc);
+    int win_w = rc.right - rc.left;
+    int win_h = rc.bottom - rc.top;
+
+    int screen_w = GetSystemMetrics(SM_CXSCREEN);
+    int screen_h = GetSystemMetrics(SM_CYSCREEN);
+
+    SetWindowPos(hwnd, HWND_TOP, (screen_w - win_w) / 2,
+        (screen_h - win_h) / 2, 0, 0, SWP_NOSIZE);
+}
+
+void AddMenus(HWND hwnd) {
+
+    HMENU hMenubar;
+    HMENU hMenu;
+
+    hMenubar = CreateMenu();
+    hMenu = CreateMenu();
+
+    AppendMenuW(hMenu, MF_STRING, IDM_NEW, L"&New          (ALT + N)");
+    AppendMenuW(hMenu, MF_STRING, IDM_FULL, L"&Full screen");
+    AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
+    AppendMenuW(hMenu, MF_STRING, IDM_QUIT, L"&Quit         (ALT + Q)");
+
+    AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&Game menu");
+    SetMenu(hwnd, hMenubar);
+}
+
+void WinShow(HDC dc) {
+    SelectObject(dc, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(dc, RGB(15, 0, 12));
+    Rectangle(dc, 0, 0, 800, 600);
+    SelectObject(dc, GetStockObject(DC_PEN));
+    SetDCBrushColor(dc, RGB(200, 0, 0));
+    for (int i = 0; i < masSize; i++)
+    {
+        Rectangle(dc, mas[i].left, mas[i].top, mas[i].right, mas[i].bottom);
+    }
+}
+
+void InitCoordinates() {
+    srand(13);
+    for (int i = 0; i < masSize; i++)
+    {
+        mas[i].left = rand() % 800 - 50;
+        mas[i].top = rand() % 400;
+        mas[i].right = mas[i].left + 50;
+        mas[i].bottom = mas[i].top + 50;
+    }
+}
+
+void ItemMove(){
+    for (int i = 0; i < masSize; i++)
+    {
+        mas[i].left += pixelMove;     
+        if(mas[i].left > 750) mas[i].left = -50;
+        mas[i].right = mas[i].left + 50;
+        mas[i].bottom = mas[i].top + 50;
+    }
 }
