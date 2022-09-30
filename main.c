c299097ca2c19dbb4ee34758e8c493adb17aa67f
Pavel Gerasimchik <60985318+pavelgerasimchik20@users.noreply.github.com>
2022-09-30 22:59:20 +0300

    PR 9/30/22 (#5)
    * add wWinMain function to create fisrt window
    
    * add CenterWindow function
    
    * registered hot key ALT+Q to close window
    
    * add menu + hotkey for each menu item
    
    * feature: Full screen menu point
    
    * add custom animation functions
    
    * main loop refactoring : created quit from infinity loop
    
    * refactoring: fun WinShow - added buffer as virtual space and virtual bitmap to draw without blinking
    
    * add structures to locating and constructing of object
    
    * -refactored structures to create hero
    -added fun which init and draw hero
    (old animation was commented)
    
    * added posible to move our hero
    
    * added object moving
    
    * removed full screen feature and system full screen button
    
    * Delete slnx.sqlite

--- main.c
+++ main.c
@@ -10,16 +10,35 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #define IDM_NEW 1
 #define IDM_FULL 2
 #define IDM_QUIT 3
-#define masSize 5
-#define pixelMove 10 // this var will responce for speed of obj   ***TODO pick mode ???***
-RECT mas[masSize];
+#define clientAreaHor 1200
+#define clientAreaVert 700
+#define pixelMove 5 // this var will responce for speed of obj   ***TODO pick mode ???***
+RECT clientArea; 
+
+typedef struct SPoint {
+    float x, y;
+}TPoint;
+
+typedef struct SObject {
+    TPoint pos;
+    TPoint size;
+    TPoint speed;
+    COLORREF brush;
+}TObject;
+
+TObject player;
+
+TPoint point(float x, float y);
+void ObjectInit(TObject* obj, float x, float y, float width, float height);
+void ObjectShow(TObject obj, HDC dc);
+void WinInit(void);
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void CenterWindow(HWND);
 void AddMenus(HWND);
-void WinShow(HDC dc);
-void InitCoordinates(void);
-void ItemMove(void);
+void WinShow(HDC dc, int hor, int vert);
+void WinMove(void);
+void ObjectMove(TObject* obj);
 
 int main(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PWSTR pCmdLine, int nCmdShow) {
@@ -41,33 +60,42 @@ int main(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
     RegisterClassW(&wc);
     hwnd = CreateWindowW(wc.lpszClassName, L"Catch me if you can",
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        0, 0, 800, 600, NULL, NULL, hInstance, NULL);
+        WS_OVERLAPPEDWINDOW & (~WS_MAXIMIZEBOX) | WS_VISIBLE,
+        0, 0, clientAreaHor, clientAreaVert, NULL, NULL, hInstance, NULL);
     HDC dc = GetDC(hwnd);  // getting context of the window
 
     ShowWindow(hwnd, SW_SHOWNORMAL);
     UpdateWindow(hwnd);
-    InitCoordinates();
+    WinInit();  
 
+    Sleep(3000); // show welcome page
     while (1) {
-        if (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE)) { // check the mess queue
+        if (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE)) { // check the messages queue
+            if (msg.message == WM_QUIT) break;
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
         else {
-            ItemMove();
-            WinShow(dc);
+            WinMove();
+            WinShow(dc,clientAreaHor,clientAreaVert);
             Sleep(1);
         }
-        //printf("msg.message: %d & msg.wParam: %d\n", msg.message, msg.wParam);  that needs to spy on codes of everything
+        printf("msg.message: %d & msg.wParam: %d\n", msg.message, msg.wParam);  //that needs to spy on action codes of messages 
     }
-   
     return 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
     WPARAM wParam, LPARAM lParam) {
 
+    static HBITMAP hbtm;
+    HDC hdc;
+    PAINTSTRUCT ps;
+    BITMAP bitmap;
+    HDC hdcMem;
+    HGDIOBJ oldBitmap;
+
+    if (msg == WM_SIZE) GetClientRect(hwnd, &clientArea);
     switch (msg) 
     {
     case WM_CREATE:
@@ -77,19 +105,39 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
         CenterWindow(hwnd);
         break;
 
+    case WM_PAINT:
+        hbtm = (HBITMAP)LoadImageW(NULL, L"D:\\Pictures\\welcome.bmp",
+            IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+        hdc = BeginPaint(hwnd, &ps);
+        hdcMem = CreateCompatibleDC(hdc);
+        oldBitmap = SelectObject(hdcMem, hbtm);
+        GetObject(hbtm, sizeof(bitmap), &bitmap);
+        BitBlt(hdc, 5, 5, bitmap.bmWidth, bitmap.bmHeight,
+            hdcMem, 0, 0, SRCCOPY);
+        SelectObject(hdcMem, oldBitmap);
+        DeleteDC(hdcMem);
+        EndPaint(hwnd, &ps);
+        break;
+
     case WM_COMMAND:
         switch (LOWORD(wParam)) {
         case IDM_NEW:
             MessageBeep(MB_ICONINFORMATION);
-            break;
-        case IDM_FULL:
-            ShowWindow(hwnd, SW_MAXIMIZE);
             break;
         case IDM_QUIT:
             SendMessage(hwnd, WM_CLOSE, 0, 0);
             break;
         }
         return 0;
+    case WM_KEYDOWN:
+        if (wParam == VK_ESCAPE) {
+            int ret = MessageBoxW(hwnd, L"Are you sure to quit?",
+                L"Message", MB_OKCANCEL);
+            if (ret == IDOK) {
+                SendMessage(hwnd, WM_CLOSE, 0, 0);
+            }
+        }
+        break;
 
     case WM_HOTKEY:
         if ((wParam) == ID_HOTKEY_QUIT) {
@@ -100,6 +148,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
         }
         break;
     case WM_DESTROY:
+        DeleteObject(hbtm);
         UnregisterHotKey(hwnd, ID_HOTKEY_QUIT);
         UnregisterHotKey(hwnd, ID_HOTKEY_NEW);
         PostQuitMessage(EXIT_SUCCESS);    
@@ -132,7 +181,7 @@ void AddMenus(HWND hwnd) {
     hMenu = CreateMenu();
 
     AppendMenuW(hMenu, MF_STRING, IDM_NEW, L"&New          (ALT + N)");
-    AppendMenuW(hMenu, MF_STRING, IDM_FULL, L"&Full screen");
+    //AppendMenuW(hMenu, MF_STRING, IDM_FULL, L"&Full screen");
     AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
     AppendMenuW(hMenu, MF_STRING, IDM_QUIT, L"&Quit         (ALT + Q)");
 
@@ -140,35 +189,71 @@ void AddMenus(HWND hwnd) {
     SetMenu(hwnd, hMenubar);
 }
 
-void WinShow(HDC dc) {
-    SelectObject(dc, GetStockObject(DC_BRUSH));
-    SetDCBrushColor(dc, RGB(15, 0, 12));
-    Rectangle(dc, 0, 0, 800, 600);
+void WinShow(HDC dc, int hor, int vert) {
+
+    HDC virtualDC = CreateCompatibleDC(dc); // virtual context
+    HBITMAP virtualBITMAP = CreateCompatibleBitmap(dc, clientArea.right - clientArea.left, clientArea.bottom - clientArea.top);
+    SelectObject(virtualDC, virtualBITMAP); // so in a context virtualDC I will draw on the picture virtualBITMAP
+        SelectObject(virtualDC, GetStockObject(DC_BRUSH));
+        SetDCBrushColor(virtualDC, RGB(25, 0, 12));
+        Rectangle(virtualDC, 0, 0, hor, vert);
+
+        ObjectShow(player, virtualDC);
+        /*SelectObject(virtualDC, GetStockObject(DC_PEN));
+        SetDCBrushColor(virtualDC, RGB(100, 0, 0));
+        for (int i = 0; i < masSize; i++)
+        {
+            Rectangle(virtualDC, mas[i].left, mas[i].top, mas[i].right, mas[i].bottom);
+        }*/
+        BitBlt(dc, 0, 0, clientArea.right - clientArea.left, clientArea.bottom - clientArea.top, virtualDC, 0, 0, SRCCOPY);
+        DeleteDC(virtualDC);
+        DeleteObject(virtualBITMAP);
+}
+void PlayerControl() {
+    static float playerSpeed = 4.0;
+    player.speed.x = 0;
+    player.speed.y = 0;
+    if (GetKeyState('W') < 0) player.speed.y = -playerSpeed;
+    if (GetKeyState('S') < 0) player.speed.y = playerSpeed;
+    if (GetKeyState('A') < 0) player.speed.x = -playerSpeed;
+    if (GetKeyState('D') < 0) player.speed.x = playerSpeed;
+    if ((player.speed.x != 0) && (player.speed.y != 0))
+        player.speed = point(player.speed.x * 0.7f, player.speed.y * 0.7f);
+}
+
+void WinMove(){
+    PlayerControl();
+    ObjectMove(&player);
+}
+
+TPoint point(float x, float y) {
+    TPoint point;
+    point.x = x;
+    point.y = y;
+    return point;
+}
+
+void ObjectInit(TObject* obj, float x, float y, float width, float height) {
+    obj->pos = point(x, y);
+    obj->speed = point(0, 0);
+    obj->size = point(width, height);
+    obj->brush = RGB(200, 150, 30);
+}
+
+void ObjectShow(TObject obj, HDC dc) {
     SelectObject(dc, GetStockObject(DC_PEN));
-    SetDCBrushColor(dc, RGB(200, 0, 0));
-    for (int i = 0; i < masSize; i++)
-    {
-        Rectangle(dc, mas[i].left, mas[i].top, mas[i].right, mas[i].bottom);
-    }
+    SetDCPenColor(dc, RGB(0, 0, 0));
+    SelectObject(dc, GetStockObject(DC_BRUSH));
+    SetDCBrushColor(dc, obj.brush);
+    Rectangle(dc, (int)(obj.pos.x), (int)(obj.pos.y),
+        (int)(obj.pos.x + obj.size.x), (int)(obj.pos.y + obj.size.y));
 }
 
-void InitCoordinates() {
-    srand(13);
-    for (int i = 0; i < masSize; i++)
-    {
-        mas[i].left = rand() % 800 - 50;
-        mas[i].top = rand() % 400;
-        mas[i].right = mas[i].left + 50;
-        mas[i].bottom = mas[i].top + 50;
-    }
+void WinInit() {
+    ObjectInit(&player, 100, 100, 40, 40);
 }
 
-void ItemMove(){
-    for (int i = 0; i < masSize; i++)
-    {
-        mas[i].left += pixelMove;     
-        if(mas[i].left > 750) mas[i].left = -50;
-        mas[i].right = mas[i].left + 50;
-        mas[i].bottom = mas[i].top + 50;
-    }
+void ObjectMove(TObject* obj) {
+    obj->pos.x += obj->speed.x;
+    obj->pos.y += obj->speed.y;
 }
